236a6a87e0e7abd2b7554d871e9d4c660c01109e
migueldecode <migueldecode@gmail.com>
2023-01-13 21:48:53 +0100

    Save files

---
+++ 01-2023/goals-2023/js/index.js
@@ -0,0 +1,42 @@
+const goalsContainer = document.querySelector(".goals__container");
+const boxes = document.querySelectorAll(".box__goal");
+const inputs = document.querySelectorAll(".box__input");
+const days = document.querySelectorAll(".calendar li");
+
+const createGoal = (goal) => {
+  const box = document.createElement("section");
+  box.classList.add("box__goal");
+  box.innerHTML = `
+              <h2 class="box__title">Meta</h2>
+              <div class="box__input"></div>
+              <img class="box__delete" src="./assets/delete.webp" alt="delete"></img>
+              <p class="box__text">${goal}.</p>
+    `;
+  goalsContainer.appendChild(box);
+};
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Enter") {
+    createGoal(event.target.value);
+    event.target.value = "";
+  }
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.value === "box__delete") {
+    event.target.closest(".box__goal").outerHTML = "";
+  }
+
+  if (
+    event.target.classList.value === "box__input" ||
+    event.target.classList.value === "box__input done"
+  ) {
+    event.target.classList.toggle("done");
+  }
+
+  days.forEach((day) => {
+    if (event.target === day) {
+      event.target.classList.add("active");
+    }
+  });
+});
--- 01-2023/tres-en-raya/index.js
+++ 01-2023/tres-en-raya/index.js
@@ -1,5 +1,6 @@
 const game = document.getElementById("game");
 const areas = document.querySelectorAll(".game__area");
+const modal = document.querySelector("dialog");
 
 let player = 1;
 let isOn = true;
@@ -10,57 +11,65 @@ document.addEventListener("click", (event) => {
   if (isOn) {
     for (let i = 0; i < areas.length; i++) {
       if (event.target === areas[i]) {
+        if (event.target.textContent !== "") return;
+
         if (player === 1) {
           event.target.textContent = cross;
           event.target.classList.add("player-one");
           player = 2;
-          checkWinner();
+          checkWinner(1);
         } else {
           event.target.textContent = circle;
           event.target.classList.add("player-two");
           player = 1;
-          checkWinner();
+          checkWinner(2);
         }
       }
     }
   }
 });
 
-function checkWinner() {
+function checkWinner(player) {
   const table = Array.from(areas).map((area) => area.textContent);
 
   // Review Rows
   for (let i = 0; i <= 9; i += 3) {
     if (table[i] && table[i] === table[i + 1] && table[i] === table[i + 2]) {
-      return winner([i, i + 1, i + 2]);
+      return winner([i, i + 1, i + 2], player);
     }
   }
 
   // Review Columns
   for (let i = 0; i <= 3; i++) {
     if (table[i] && table[i] === table[i + 3] && table[i] === table[i + 6]) {
-      return winner([i, i + 3, i + 6]);
+      return winner([i, i + 3, i + 6], player);
     }
   }
 
   // Review Cross
   if (table[0] && table[0] === table[4] && table[0] === table[8]) {
-    return winner([0, 4, 8]);
+    return winner([0, 4, 8], player);
   }
 
   if (table[2] && table[2] === table[4] && table[2] === table[6]) {
-    return winner([2, 4, 6]);
+    return winner([2, 4, 6], player);
   }
 
   if (table.includes("")) return;
 
-  return console.log("Draw");
+  return showModal("Empate entre los jugadores");
 }
 
-function winner(winnerCombination) {
+function winner(winnerCombination, player) {
   isOn = false;
-  console.log("ganador", winnerCombination);
   winnerCombination.forEach((pos) => {
     areas[pos].classList.add("winner");
   });
+  let message = `The winner player is ${player} and the combination was ${winnerCombination}`;
+  showModal(message);
 }
+
+const showModal = (message) => {
+  modal.textContent = message;
+  modal.showModal();
+};
