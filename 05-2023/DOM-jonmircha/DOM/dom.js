95c0e39afe66025ebcdde66abbdb215172cbf755
Miguel Decode <migueldecode@gmail.com>
2023-05-01 16:30:07 +0200

    Finish the new course of VsCode from Fernando Herrera and start a new month

---
+++ 05-2023/DOM-jonmircha/DOM/dom.js
@@ -0,0 +1,14 @@
+// INTRODUCCIÓN AL DOM
+
+console.log(window.document);
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.doctype);
+console.log(document.charset);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.scripts);
