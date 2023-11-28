93564e4142588be7624701ec763e43208ec94ccf
Miguel Decode <migueldecode@gmail.com>
2023-11-28 18:23:14 +0100

    feat: add dynamic options to the select

--- 11-2023/exchange-calculator/script.js
+++ 11-2023/exchange-calculator/script.js
@@ -5,15 +5,39 @@ const amountElementTwo = document.getElementById("amount-two");
 const rateElement = document.getElementById("rate");
 const swapBtn = document.getElementById("swap");
 
+const url = "https://open.exchangerate-api.com/v6/latest";
+
+const createOptions = () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const options = Object.keys(data.rates);
+      options.forEach((el) => {
+        const option = document.createElement("option");
+        option.textContent = el;
+        option.value = el;
+        if(el === 'USD') option.setAttribute('selected', 'selected')
+        currencyElementOne.appendChild(option);
+      });
+      options.forEach((el) => {
+        const option = document.createElement("option");
+        option.textContent = el;
+        option.value = el;
+        if (el === "EUR") option.setAttribute("selected", "selected");
+        currencyElementTwo.appendChild(option);
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
 const calculate = () => {
-  const url = "https://open.exchangerate-api.com/v6/latest";
   const currencyOne = currencyElementOne.value;
   const currencyTwo = currencyElementTwo.value;
 
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      // console.log(data);
+      // console.log(data.rates);
       const rate = data.rates[currencyTwo] / data.rates[currencyOne];
       rateElement.innerHTML = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
       amountElementTwo.value = (amountElementOne.value * rate).toFixed(2);
@@ -28,6 +52,8 @@ const swapCurrencies = () => {
 };
 
 // Event Listeners
+document.addEventListener("DOMContentLoaded", createOptions);
+
 currencyElementOne.addEventListener("change", calculate);
 amountElementOne.addEventListener("input", calculate);
 currencyElementTwo.addEventListener("change", calculate);
