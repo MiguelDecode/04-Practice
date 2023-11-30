dc7978e653c1124b3a81de01d9e139d9e7991c9c
Miguel Decode <migueldecode@gmail.com>
2023-11-30 16:55:18 +0100

    study: collapse sidebar menu

---
+++ 11-2023/case-study/collapse-sidebar/script.js
@@ -0,0 +1,16 @@
+const btnClose = document.getElementById("btn-close");
+const btnOpen = document.getElementById("btn-open");
+
+const openNav = () => {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+};
+
+// Event Listeners
+btnClose.addEventListener("click", closeNav);
+btnOpen.addEventListener("click", openNav);
