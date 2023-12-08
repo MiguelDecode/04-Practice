00587521e74372b713f167196651b57aff716e96
migueldecode <migueldecode@gmail.com>
2023-12-08 15:38:55 +0100

    feat: add animation to code the message

--- 12-2023/cesar-cipher/script.js
+++ 12-2023/cesar-cipher/script.js
@@ -8,7 +8,6 @@ let value = 10;
 
 const alphabet = "abcdefghijklmnñopqrstuvwxyz".split("");
 
-
 const handleOutput = (event) => {
   value = rangeCipher.value;
 
@@ -22,13 +21,35 @@ const codeMessage = (currentLetterIndex, arrMessage) => {
 
   const spanCode = document.createElement("span");
   resultCipher.appendChild(spanCode);
-  const originalLetter = arrMessage[currentLetterIndex];
 
-  spanCode.innerHTML = alphabet.includes(originalLetter)
-    ? alphabet[alphabet.indexOf(originalLetter) + (Number(value) % alphabet.length)]
-    : originalLetter;
+  animateLetter(spanCode).then(() => {
+    const originalLetter = arrMessage[currentLetterIndex];
 
-  codeMessage(currentLetterIndex + 1, arrMessage);
+    spanCode.innerHTML = alphabet.includes(originalLetter)
+      ? alphabet[
+          (alphabet.indexOf(originalLetter) + parseInt(value)) % alphabet.length
+        ]
+      : originalLetter;
+
+    codeMessage(currentLetterIndex + 1, arrMessage);
+  });
+};
+
+const animateLetter = (element) => {
+  let changesOfLetter = 0;
+
+  return new Promise((resolve) => {
+    const roulette = setInterval(() => {
+      element.innerHTML = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+      changesOfLetter++;
+
+      if (changesOfLetter === 3) {
+        clearInterval(roulette);
+        resolve();
+      }
+    }, 50);
+  });
 };
 
 const takeInputMessage = () => {
